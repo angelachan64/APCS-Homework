22ef8ac463179d76966065f7c92081c939271184
angelachan64 <angelachan64@gmail.com>
2014-09-15 08:41:26 -0400

    Driver

--- 02-greeter/Greeter.java
+++ 02-greeter/Greeter.java
@@ -6,8 +6,21 @@ import java.util.*;
 //CODE
 
 public class Greeter {
+    /*
+      public <- can be used outside of the class
+      void <- this DOES NOT return anything you would place a
+              "return type" here if the method did return something
+      greet <- name
+      () <- parameters go here
+      {} <- code goes here
+    */
     public void greet() {
 	System.out.println("Hello world!");
+	ungreet();
+    }
+
+    private void ungreet() {
+	System.out.println("I'm out");
     }
 }
 
