9d99593a7ff4c03b91ac0e766383cff60919afc0
angelachan64 <angelachan64@gmail.com>
2014-09-22 19:31:50 -0400

    Homework on string method practice

---
+++ 05-strings/Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+    public static void main(String[]args){
+	mixStart s = new mixStart("pixels");
+	makeOutWord m = new makeOutWord("<<>>","Hehehehe");
+	firstHalf f = new firstHalf("Racecars");
+	s.test1();
+        m.test1();
+	f.test1();
+    }
+}
---
+++ 05-strings/firstHalf.java
@@ -0,0 +1,5 @@
+public String firstHalf(String str) {
+  int len = str.length();
+  int half = (len/2);
+  return str.substring(0, half);
+}
