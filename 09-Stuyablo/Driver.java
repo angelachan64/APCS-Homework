4b026165d9f935f3f249c7f5cc48a0025e68c832
angelachan64 <angelachan64@gmail.com>
2014-10-14 08:29:34 -0400

    Stuyablo learning necessities

---
+++ 09-Stuyablo/Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+    public static void main(String[]args){
+	//Random r = new Random();
+	for (int i=0;i<10;i++){
+	    int val = (int)(Math.random()*100);
+	    System.out.println(val);
+	}
+    }
+}
