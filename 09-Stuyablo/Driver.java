f889d204f0fd7e3c2c934eb01622491702bdd66a
angelachan64 <angelachan64@gmail.com>
2014-10-14 08:40:32 -0400

    new Stuyablo necessities

--- 09-Stuyablo/Driver.java
+++ 09-Stuyablo/Driver.java
@@ -1,9 +1,19 @@
+import java.util.*;
+
 public class Driver{
     public static void main(String[]args){
-	//Random r = new Random();
+	Random r = new Random(10);
 	for (int i=0;i<10;i++){
-	    int val = (int)(Math.random()*100);
-	    System.out.println(val);
+	    //int val = (int)(Math.random()*100);
+	    //System.out.println(val);
+	    System.out.println(r.nextInt(100));
+	}
+	Scanner sc = new Scanner(System.in);
+	String input = "";
+	while (!input.equals("done")){
+	    System.out.println("Enter a value:");	    
+	    input = sc.nextLine();
+	    System.out.println(input);
 	}
     }
 }
