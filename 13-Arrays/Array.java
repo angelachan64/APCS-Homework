de8fb840ece9edd3b591f76e759662a5f08d9c92
angelachan64 <angelachan64@gmail.com>
2014-10-23 22:06:52 -0400

    Array homework

--- 13-Arrays/Array.java
+++ 13-Arrays/Array.java
@@ -10,24 +10,24 @@ public class Array{
 	    array[1] = nums[1];
 	    return array;
 	}
-	public int sum13(int[] nums){
-	    if (nums.length == 0){
-		return 0;
-	    }
-	    else{
-		int x = 1;
-		int sum = 0;
-		while (x <= nums.length){
-		    if (nums[x - 1] != 13){
-			sum = sum + nums[x - 1];
-		    }
-		    else{
-			x = x + 1;
-		    }
+    }
+    public int sum13(int[] nums){
+	if (nums.length == 0){
+	    return 0;
+	}
+	else{
+	    int x = 1;
+	    int sum = 0;
+	    while (x <= nums.length){
+		if (nums[x - 1] != 13){
+		    sum = sum + nums[x - 1];
+		}
+		else{
 		    x = x + 1;
 		}
-		return sum;
+		x = x + 1;
 	    }
+	    return sum;
 	}
     }
 }
