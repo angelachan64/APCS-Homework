5f4e81b141e99d5fedba744bf3a63e1c3252d9cd
angelachan64 <angelachan64@gmail.com>
2014-11-10 08:32:21 -0500

    try catch block stuff

---
+++ 14-Errors/Eexample.java
@@ -0,0 +1,9 @@
+public class Eexample {
+    public void etest(int i){
+	if (i>10){
+	    //throw new ArrayIndexOutOfBoundsException();
+	    throw new ArithmeticException();
+	}
+	System.out.println("We're okay");
+    }
+}
