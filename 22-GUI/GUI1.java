bd2aef267f4ad54af358338d611eefff79afa724
angelachan64 <angelachan64@gmail.com>
2014-12-16 08:40:34 -0500

    some of the stuff doesn't work

--- 22-GUI/GUI1.java
+++ 22-GUI/GUI1.java
@@ -2,11 +2,45 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GUI1 extends JFrame{
+    private Container pane;
+    private JButton b1,b2;
+    private JLabel l;
+    private JTextArea text;
+    private JPanel canvas;
     public GUI1(){
 	setTitle("My First GUI");
-	setSize(600,400);
+	setSize(600,600);
 	setLocation(100,100);
-	setDefaultCloseOPeration(EXIT_ON_CLOSE);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	
+	pane = getContentPane();
+	pane.setLayout(new FlowLayout(3));
+	l = new JLabel("The Label:");
+	b1 = new JButton("button");
+	pane.add(b1);
+	b2 = new JButton("exit");
+	pane.add(b2);
+	
+	text = new JTextArea("HELLO");
+	text.setColumns(40);
+	text.setRows(10);
+	text.setBorder(BorderFactory.createLineBorder(Color.red,2));
+	pane.add(text);
+
+	canvas = new JPanel();
+	canvas.setPreferredSize(new Dimension(300,300));
+	canvas.setBorder(BorderFactory.createLineBorder(Color.blue,2));
+	pane.add(canvas);
+	
+	/*pane.add(new JButton("hello1"));
+	pane.add(new JButton("hello2"));
+	pane.add(new JButton("hello3"));
+	JPanel f2 = new JPanel();
+	pane.add(f2);
+	f2.getContentPane().setLayout(new GridLayout(3,3));
+	f2.getContentPane().add(new JButton("aaa"));
+	f2.getContentPane().add(new JButton("aaa"));
+	f2.getContentPane().add(new JButton("aaa"));*/
     }
     public static void main(String[] args){
 	GUI1 f = new GUI1();
